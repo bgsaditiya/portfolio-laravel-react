b2640a8ea7022bfccba74ef5f6c17db81ee5a3a7
Bagas Aditia <bagasaditiya11@gmail.com>
2025-01-16 14:29:23 +0700

    initial commit

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,11 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="{{ asset('/images/logo.png') }}" type="image/png">
     @viteReactRefresh
     @vite('resources/js/app.jsx')
     @vite('resources/css/app.css')
     @inertiaHead
-    <title>Document</title>
+    <title>Portfolio | Bagas Aditia</title>
 </head>
 
 <body class="bg-main">
